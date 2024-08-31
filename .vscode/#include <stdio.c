b8d39452b2bc71597484035fca4367d2a0990d7e
pratham092006 <pinglepratham618@gmail.com>
2024-08-31 04:09:39 +0000

    largest array

---
+++ .vscode/#include <stdio.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <math.h>
+
+
+
+int main() {
+
+    double base = 2.0;
+
+    double exponent = 3.0;
+
+    double result = pow(base, exponent);
+
+    printf("Power: %.2lf^%.2lf = %.2lf\n", base, exponent, result);
+
+    return 0;
+
+}
+
+ 
